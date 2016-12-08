95f5065ee2c0f7fbc70a1d70c78682b51aeef130
MuhamadOmr <mohamadomr33@gmail.com>
2016-12-08 23:11:47 +0200

    saf

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+    <h1>Create Posts</h1>
+
+
+@stop
---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+    <h1>Posts</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Category</th>
+              <th>Photo</th>
+              <th>Title</th>
+              <th>body</th>
+              <th>created</th>
+              <th>updated</th>
+
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($posts)
+
+            @foreach($posts as $post)
+          <tr>
+            <td>{{$post->id}}</td>
+            <td>{{$post->user_id}}</td>
+            <td>{{$post->category_id}}</td>
+             <td>{{$post->photo_id}}</td>
+             <td>{{$post->title}}</td>
+             <td>{{$post->body}}</td>
+              <td>{{$post->created_at->diffForHumans()}}</td>
+              <td>{{$post->updated_at->diffForHumans()}}</td>
+          </tr>
+            @endforeach
+            @endif
+        </tbody>
+      </table>
+
+
+    @stop
